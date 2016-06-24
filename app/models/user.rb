d3d5db714f041253b283bf3f7b8b234339358ab2
Marcel Sands <marcel.sands84@gmail.com>
2016-06-24 10:59:40 -0400

    add invitable to schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable, :timeoutable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+  devise :invitable, :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
 
   validates :username, :presence => true, :uniqueness => { :case_sensitive => false }
 
