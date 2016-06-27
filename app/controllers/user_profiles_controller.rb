6908945eca1b3bd5a4a9ddf1c4a9cd2e91cbc257
Marcel Sands <marcel.sands84@gmail.com>
2016-06-27 16:34:27 -0400

    add logic to UserProfile controller

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -1,2 +1,45 @@
 class UserProfilesController < ApplicationController
+
+  def index
+    @user_profiles = UserProfile.all
+  end
+
+  def show
+    @user_profile = UserProfile.find(params[:id])
+  end
+
+  def new
+    @user_profile = UserProfile.new
+  end
+
+  def edit
+    @user_profile = UserProfile.find(params[:id])
+  end
+
+  def create
+    @user_profile = UserProfile.new(user_profile_params)
+    @user_profile.user = current_user
+    if @user_profile.save
+      redirect_to my_profiles_path, notice: "Profile successfully saved!"
+    else
+      flash[:error] = "Oops, something went wrong"
+      render "new"
+    end
+  end
+
+  def update
+    @user_profile = UserProfile.find(params[:id])
+    if @user_profile.update_attributes(user_profile_params)
+      flash[:success] = "Profile updated"
+      redirect_to my_profiles_path
+    else
+      render "edit"
+    end
+  end
+
+  protected
+
+  def user_profile_params
+    params.require(:user_profile).permit(:church, :state, :website, :birthday, :gender, interests: [])
+  end
 end
