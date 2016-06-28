f7f9310792a4b685227fd6defc84234f4b175277
Marcel Sands <marcel.sands84@gmail.com>
2016-06-28 13:12:57 -0400

    create user_profile form and add logic for models

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,2 +1,10 @@
 class UserProfile < ActiveRecord::Base
+
+  INTERESTS = ["Gaming", "Photo Editing", "Web Design/Development", "Servers", "Computer Repair"]
+
+  belongs_to :user
+
+  acts_as_birthday :birthday
+
+  delegate :email, to: :user
 end
