bfdaef330d0380a115a2a5d899e6957c1d2458db
Marcel Sands <marcel.sands84@gmail.com>
2016-07-31 22:31:30 -0400

    add logic to support controller new and create action

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -1,5 +1,16 @@
 class SupportsController < ApplicationController
   def new
+    @support = Support.new
+  end
+
+  def create
+    @support = Support.new(support_params)
+    if @support.save
+      redirect_to root_path, notice: "Your support ticket has been submitted!"
+    else
+      flash[:error] = "Something's not right; please resubmit"
+      render "new"
+    end
   end
 
   protected
