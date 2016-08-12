50db13fd29a82a5c442cf4b3be51551732e98e2c
Marcel Sands <marcel.sands84@gmail.com>
2016-08-12 00:12:07 -0400

    add basics for event controller params and index action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,17 @@
 class EventsController < ApplicationController
+  before_action :find_event, only: [:show, :edit, :update, :destroy]
+
   def index
+    @events = Event.all
+  end
+
+  protected
+
+  def find_event
+    @event = Event.find(params[:id])
+  end
+
+  def events_params
+    params.requrie(:event).permit(:title, :description, :date, :time_start, :time_end, :address, :state, :zip_code, :event_organizer, :dress_code, :price, :food_provided, :notes)
   end
 end
